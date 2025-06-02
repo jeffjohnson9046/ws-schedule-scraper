a33fcf28b40a31403cacc97bdb5d8a5cf0d649f0
jeffjohnson9046 <jeffjohnson9046@hotmail.com>
2025-06-01 20:35:01 -0700

    Adjust event date format

--- dto/showinfo.go
+++ dto/showinfo.go
@@ -22,6 +22,8 @@ func (showInfo *ShowInfo) ToScheduleEvent() ScheduleEvent {
 		log.Fatalf("Unable to parse date for event in correct format. got=%s", showInfo.Date)
 	}
 
-	formattedDate := startDate.Format("2006-01-01")
+	// TODO: pretty sure startDate.Format(-- the pattern --) should work here, but startDate.format("2006-01-01") kept giving back the same date, regardless of input
+	formattedDate := fmt.Sprintf("%d-%02d-%02d", startDate.Year(), startDate.Month(), startDate.Day())
+
 	return ScheduleEvent{Summary: showInfo.String(), DateTime: formattedDate}
 }
