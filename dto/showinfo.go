9be47996c5bab08620b5d1c446446ba7c7b2e909
jeffjohnson9046 <jeffjohnson9046@hotmail.com>
2025-06-02 06:42:58 -0700

    Remove TODO

--- dto/showinfo.go
+++ dto/showinfo.go
@@ -22,7 +22,6 @@ func (showInfo *ShowInfo) ToScheduleEvent() ScheduleEvent {
 		log.Fatalf("Unable to parse date for event in correct format. got=%s", showInfo.Date)
 	}
 
-	// TODO: pretty sure startDate.Format(-- the pattern --) should work here, but startDate.format("2006-01-01") kept giving back the same date, regardless of input
 	formattedDate := startDate.Format("2006-01-02")
 
 	return ScheduleEvent{Summary: showInfo.String(), DateTime: formattedDate}
